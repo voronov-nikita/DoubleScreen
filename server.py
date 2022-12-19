4feba2df60d26a3ff54e84961c8b9ddd50f5d5ed
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2022-12-19 22:38:48 +0300

    code

--- server.py
+++ server.py
@@ -1,57 +1,81 @@
 import socket
 
-from os import getlogin
-
 from PIL import Image  # изображение
-import io
-import numpy as np
-from random import randint
-import pyautogui    #много назначений
+
+import pyautogui  # много назначений
 
 from threading import Thread  # потоки
 
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QPushButton, QAction, QMessageBox
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import QRect, Qt
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = socket.gethostbyname_ex(socket.gethostname())[-1][-1]
 PORT = 9999
 
 print("STARTED")
-print(IP)
-print(PORT)
-sock = socket.socket()
+print(f"IP-adress: {IP}")
+print(f"PORT-connected: {PORT}")
+sock = socket.socket()  # создаем сокет
 sock.bind((IP, PORT))  # к серверу
-sock.listen(1)  # максимум одно подключение
+sock.listen()
 conn, addr = sock.accept()
 
 
 class Dekstop(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.init_UI()
+        self.initUI()
+        self.mouse_x, self.mouse_y = map(int, pyautogui.position())
 
     def ChangeImage(self):
         try:
-            print("[SERVER]: CONNECTED: {0}!".format(addr[0]))
             while True:
-                img_bytes = conn.recv(9999999)
-                self.pixmap.loadFromData(img_bytes)
-                self.label.setScaledContents(True)
-                self.label.resize(self.width(), self.height())
-                self.label.setPixmap(self.pixmap)
+                rmouseclk = False  # обнуляем проверку на клик правой кнопкой
+                lmouseclk = False  # обнуляем проверку на клик левой кнопкой
+                data = conn.recv(99999999)  # Принимаем данные с клиента
+                try:
+                    if data.decode('utf-8'):  # проверка можно ли декодировать
+
+                        # <-----------Проверить на получение нажатия----------->
+                        if data.decode('utf-8') == "RMouseClick":  # проверка нажатия правой кнопки мыши
+                            rmouseclk = True
+                        if data.decode('utf-8') == "LMouseClick":  # проверка нажатия левой кнопкой мыши
+                            lmouseclk = True
+
+                        self.mouse_x = int(data.decode('utf-8').split()[0])  # задаем значение для X
+                        self.mouse_y = int(data.decode('utf-8').split()[1])  # задаем значение для Y
+
+                except UnicodeDecodeError:
+                    full = self.pixmap.loadFromData(data)
+                    if full:
+                        self.pixmap.loadFromData(data)
+                        self.label.setScaledContents(True)
+                        self.label.resize(self.width(), self.height())
+                        self.label.setPixmap(self.pixmap)
+
+                        # <----------------Требует доработки----------------->
+                        ## if rmouseclk:
+                        ##     pyautogui.mouseDown(button='left')   # нажать на левую кнопку
+                        ## if lmouseclk:
+                        ##     pyautogui.mouseDown(button='right')  # нажать на правую кнопку
+                        # pyautogui.moveTo(self.mouse_x, self.mouse_y, 2)  # для того, чтобы менять координаты мыши на то, что выдает клиент
+
+
         except ConnectionResetError:
             QMessageBox.about(self, "ERROR", "Error Client")
             conn.close()
 
-    def init_UI(self):
+    def initUI(self):
+        self.setWindowIcon(QIcon('logo-start.png'))  # лого основного окна
         self.pixmap = QPixmap()
         self.label = QLabel(self)
         self.label.resize(self.width(), self.height())
-        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 800, 450))
+        x, y = map(int, pyautogui.size())  # размеры экрана
+        self.setGeometry(QRect(x // 4, y // 4, 800, 450))  # окно проецирования
         self.setFixedSize(self.width(), self.height())
-        self.setWindowTitle("server" + str(randint(99999, 999999)))
+        self.setWindowTitle("server")
         self.start = Thread(target=self.ChangeImage, daemon=True)
         self.start.start()
 
--- server_test.py
+++ server_test.py
@@ -1,43 +1,19 @@
 import socket
-import sys
+import json
 
 # создаём сокет и связываем его с IP-адресом и портом
 
 sock = socket.socket()
-ip = "localhost"
+ip = "192.168.0.14"
 port = 9999
 sock.bind((ip, port))
 
-# сервер ожидает передачи информации
+
 sock.listen(10)
 
 while True:
     # начинаем принимать соединения
     conn, addr = sock.accept()
 
-    # выводим информацию о подключении
-    print('connected:', addr)
-
-    # получаем название файла
-    name_f = (conn.recv(1024)).decode('UTF-8')
-
-    # открываем файл в режиме байтовой записи в отдельной папке 'sent'
-    f = open('sent/' + name_f, 'wb')
-
-    while True:
-
-        # получаем байтовые строки
-        l = conn.recv(1024)
-
-        # пишем байтовые строки в файл на сервере
-        f.write(l)
-
-        if not l:
-            break
-
-    f.close()
-    conn.close()
-
-    print('File received')
-
-sock.close()
+    ls = [1, 2, 3, 4, 5]
+    conn.send(json.dumps(ls).encode('utf-8'))
