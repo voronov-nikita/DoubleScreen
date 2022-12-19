f403abd4b1249347c3cb6c9e51920abf3189863c
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2022-12-19 22:37:23 +0300

    ignore

--- client.py
+++ client.py
@@ -1,23 +1,24 @@
 import socket
 
+# import keyboard - пока не потребуется
+import mouse
 from PIL import ImageGrab
 import io
-import numpy as np
-from random import randint
+
 import pyautogui
 
 from threading import Thread
 
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel, QPushButton, QAction, QMessageBox, QLineEdit
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import QRect, Qt
 
 
 class Dekstop(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.init_UI_Interact()
 
     def StartThread(self):
         self.start.start()
@@ -28,34 +29,50 @@ class Dekstop(QMainWindow):
                 sock = socket.socket()
                 sock.connect((self.ip.text(), int(self.port.text())))
                 while True:
-                    img = ImageGrab.grab()
+                    # <------------------Считывается и обрабатывается информация------------------>
+                    img = ImageGrab.grab()  # считываем данные экрана
                     img_bytes = io.BytesIO()
                     img.save(img_bytes, format='PNG')
-                    sock.send(img_bytes.getvalue())
-                sock.close()
-        except:
+
+                    mouse_x, mouse_y = map(int, pyautogui.position())  # считываем координа мыши
+
+                    # <------------------Отправка на Сервер------------------>
+                    sock.send(img_bytes.getvalue())  # отправляем скриншот
+
+                    sock.send(str(mouse_x).encode('utf-8'))  # отправляем координаты мыши по X
+                    sock.send(" ".encode('utf-8'))  # для разделения координат x/y
+                    sock.send(str(mouse_y).encode('utf-8'))  # отправляем координаты мыши по Y
+
+                    # <------------Нуждается в доработке------------>
+                    # if mouse.is_pressed(button="left"):
+                    #     sock.send("LMouseClick".encode('utf-8'))
+                    # if mouse.is_pressed(button="right"):
+                    #     sock.send("RMouseClick".encode('utf-8'))
+        except ConnectionResetError:
             print("DISCONNECTED")
 
-    def initUI(self):
+    def init_UI_Interact(self):
+        self.setWindowIcon(QIcon('logo-start.png'))  # лого окна приветствия
         self.pixmap = QPixmap()
-        self.label = QLabel(self)
-        self.label.resize(self.width(), self.height())
-        self.setGeometry(QRect(pyautogui.size()[0] // 4, pyautogui.size()[1] // 4, 400, 90))
+        label = QLabel(self)
+        label.resize(self.width(), self.height())
+        x, y = map(int, pyautogui.size())  # размеры экрана
+        self.setGeometry(QRect(x // 3, y // 3, 500, 100))  # окно-подключение
         self.setFixedSize(self.width(), self.height())
-        self.setWindowTitle("main" + str(randint(99999, 999999)))
+        self.setWindowTitle("CLIENT")
         self.start = Thread(target=self.ChangeImage, daemon=True)
         self.btn = QPushButton(self)
         self.btn.move(5, 55)
-        self.btn.resize(390, 30)
+        self.btn.resize(490, 50)
         self.btn.setText("Connect")
         self.btn.clicked.connect(self.StartThread)
         self.ip = QLineEdit(self)
         self.ip.move(5, 5)
-        self.ip.resize(390, 20)
+        self.ip.resize(490, 30)
         self.ip.setPlaceholderText("IP-adress")
         self.port = QLineEdit(self)
         self.port.move(5, 30)
-        self.port.resize(390, 20)
+        self.port.resize(490, 30)
         self.port.setPlaceholderText("PORT-connect")
 
 
--- client_test.py
+++ client_test.py
@@ -1,91 +1,12 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.floatlayout import FloatLayout
-
-from kivy.animation import Animation
-from kivy.clock import Clock
-from kivy.config import Config
-from kivy.uix.image import Image
-from kivy.core import audio
-from kivy.core.audio import SoundLoader
-
 import socket
+import json
+
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
+ip = "192.168.0.14"
+port = 9999
+sock.connect((ip, port))
 
-Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '500')
-Config.set('graphics', 'height', '500')
-
-
-# I set the color constants to then color the text on the buttons (this is optional)
-red = (255 / 255, 67 / 255, 67 / 255)
-green = (0 / 255, 158 / 255, 60 / 255)
-
-
-class MainApp(App):
-    def build(self):
-        # here I add the main and second screens to the manager, this class does nothing else
-        sm.add_widget(MainScreen())
-        sm.add_widget(SecondScreen())
-        return sm  # I return the manager to work with him later
-
-
-class MainScreen(Screen):
-    def __init__(self):
-        super().__init__()
-
-        self.name = 'Main'  # setting the screen name value for the screen manager
-        # (it's more convenient to call by name rather than by class)
-
-        main_layout = FloatLayout()  # creating an empty layout that's not bound to the screen
-
-        self.add_widget(main_layout)  # adding main_layout on screen
-
-        # Button
-        Go_Screen2 = Button(text='Go to Screen2',
-                            size_hint=(.5, .5),
-                            pos_hint={'center_x': .5, 'center_y': .5},
-                            color=red)
-
-        Go_Screen2.bind(on_press=self.to_second_scrn)  # setting up a button to perform an action when clicked
-
-        main_layout.add_widget(Go_Screen2)  # adding button on layout
-
-    def to_second_scrn(self, *args):
-        self.manager.current = 'Second'  # selecting the screen by name (in this case by name "Second")
-        return 0  # this line is optional
-
-
-class SecondScreen(Screen):
-    def __init__(self):
-        super().__init__()
-    # on this screen, I do everything the same as on the main screen to be able to switch back and forth
-        self.name = 'Second'
-        second_layout = FloatLayout()
-        self.add_widget(second_layout)
-
-        # Button
-        Go_Back = Button(text='Go to Main screen',
-                         size_hint=(.5, .5),
-                         pos_hint={'center_x': .5, 'center_y': .5},
-                         color=green)
-
-        Go_Back.bind(on_press=self.to_main_scrn)
-
-        second_layout.add_widget(Go_Back)
-
-    def to_main_scrn(self, *args):  # together with the click of the button, it transmits info about itself.
-        # In order not to pop up an error, I add *args to the function
-        self.manager.current = 'Main'
-        return 0
-
-
-sm = ScreenManager()  # it's necessary to create a manager variable that will collect screens and manage them
-
-if __name__ == '__main__':
-    MainApp().run()
+while True:
+    data2 = sock.recv(16384)
+    key = json.loads(data2, encoding="utf-8")
+    print(key)
