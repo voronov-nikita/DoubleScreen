bc38969f33600b8a0056dee8e8a62b78325c5fde
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-03-31 21:38:44 +0300

    test function and ignore

--- tests.py
+++ tests.py
@@ -1,33 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QDialog
 
-
-class MainWindow(QMainWindow):
+class NewWindow(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Main Window')
-        # self.setGeometry(100, 100, 300, 200)
+        # Установка размеров и заголовка нового окна
+        self.setWindowTitle("New Window")
+        self.setGeometry(100, 100, 200, 200)
 
-        button = QPushButton('Open New Window', self)
-        button.setGeometry(100, 50, 100, 50)
-        button.clicked.connect(self.open_new_window)
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Создание кнопки для открытия нового окна
+        self.button = QPushButton("Open new window", self)
+        self.button.clicked.connect(self.open_new_window)
 
     def open_new_window(self):
-        self.new_window = NewWindow()
-        self.new_window.show()
+        # Создание объекта нового окна и его отображение
+        new_window = NewWindow()
+        new_window.exec_()
 
-
-class NewWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle('New Window')
-        self.setGeometry(200, 200, 300, 200)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
