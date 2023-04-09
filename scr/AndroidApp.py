d16f114a8495ff518848d4b5d5749d324b301958
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-04-09 22:01:17 +0300

    testing-description-ignore

--- scr/AndroidApp.py
+++ scr/AndroidApp.py
@@ -1,7 +1,6 @@
 """
-pyinstaller -F --icon=logo.ico --noconsole --clean --add-data "icologo.png;." server.py
+pyinstaller -F --icon=logo.ico --noconsole --clean --add-data "../icologo.png;." server.py
 
-pyinstaller -F --icon=logo.ico --noconsole --clean --add-data "icologo.png;." client.py
+pyinstaller -F --icon=logo.ico --noconsole --clean --add-data "../icologo.png;." client.py
 """
 
-url ="..icologo.png"
